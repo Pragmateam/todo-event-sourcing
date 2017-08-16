9b618f44580724b1197b01a8b9ba880b950ac2d9
Ronaldo Rogério Pereira <ronaldotijucas@gmail.com>
2017-08-16 14:07:27 +1000

    feat(do-task): deal with a state without uuid

--- src/core/commands/do-task.js
+++ src/core/commands/do-task.js
@@ -1,11 +1,17 @@
 'use strict';
+const Either = require('data.either');
 
 module.exports = (Clock = Date) => ({ state, attributes }) => {
-  return {
+  // Usar predicado pra evitar o if /return abaixo
+  if (!state.uuid) {
+    return Either.Right('The task must exist!');
+  }
+  const event = {
     name: 'TASK_DONE',
     date: new Clock().toISOString(),
     attributes: {
       uuid: attributes.uuid
     }
   };
+  return Either.Left(event);
 };
--- src/core/commands/do-task.spec.js
+++ src/core/commands/do-task.spec.js
@@ -8,24 +8,49 @@ function fakeClock() {
 }
 
 describe('Commands - Do task', () => {
-  it('marks a task as done', () => {
+  it('marks a task as done', done => {
     const uuid = '1';
     const state = { uuid };
     const doTask = DoTask(fakeClock);
 
-    const event = doTask({
+    const either = doTask({
       state,
       attributes: {
         uuid
       }
     });
 
-    expect(event).to.eql({
-      name: 'TASK_DONE',
-      date: '2017-07-19T03:55:26.055Z',
+    const onSuccess = event => {
+      expect(event).to.eql({
+        name: 'TASK_DONE',
+        date: '2017-07-19T03:55:26.055Z',
+        attributes: {
+          uuid
+        }
+      });
+      done();
+    };
+
+    either.fold(onSuccess, done);
+  });
+
+  it('requires task to exist', done => {
+    const uuid = '1';
+    const state = {};
+    const doTask = DoTask(fakeClock);
+
+    const either = doTask({
+      state,
       attributes: {
         uuid
       }
     });
+
+    const onError = message => {
+      expect(message).to.match(/must exist/);
+      done();
+    };
+
+    either.fold(done, onError);
   });
 });
